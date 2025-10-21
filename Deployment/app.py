83f63f910e4658770749ee322a1f8217a2c50abe
Hadiya Kashif <hadiya.kashif@gmail.com>
2025-10-21 15:54:30 +0500

    deployment

--- Deployment/app.py
+++ Deployment/app.py
@@ -229,5 +229,5 @@ def analyze():
 def home():
     return jsonify({"status": "ok", "message": "Smart Health API Running"})
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+# if __name__ == "__main__":
+#     app.run(debug=True, port=5000)
